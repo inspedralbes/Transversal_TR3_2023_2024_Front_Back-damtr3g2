6467d6f5db7a55e1ccdd018a637df6bd6ff35bbf
IsmaelAlGh <a21ismalgelo@inspedralbes.cat>
2024-03-14 12:15:32 +0100

    added tailwindcss to project and test route

--- web/tr3g2/pages/index.vue
+++ web/tr3g2/pages/index.vue
@@ -4,8 +4,9 @@ const route = useRoute()
 
 <template>
   <div>
-    <h1>Nuxt Routing set up successfully!</h1>
+    <h1 class="">Nuxt Routing set up successfully!</h1>
     <p>Current route: {{ route.path }}</p>
+    <NuxtLink to="/prueba">Prueba</NuxtLink><br>
     <a href="https://nuxt.com/docs/getting-started/routing" target="_blank">Learn more about Nuxt Routing</a>
   </div>
 </template>
---
+++ web/tr3g2/pages/prueba.vue
@@ -0,0 +1,10 @@
+<script setup lang="js">
+const route = useRoute()
+</script>
+
+<template>
+    <div>
+        <h1>PRUEBA RUTAS</h1>
+        <p>Current route: {{ route.path }}</p>
+    </div>
+</template>
