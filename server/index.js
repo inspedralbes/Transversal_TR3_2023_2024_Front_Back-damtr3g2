bd32fee0b302ee04fcb3f04ab1176408316ee7c8
YbarTom <a22tomybanog@inspedralbes.cat>
2024-03-18 09:27:56 +0100

    l

--- server/index.js
+++ server/index.js
@@ -6,13 +6,14 @@ const server = createServer(app);
 const io = new Server(server);
 const PORT = 3001;
 
-io.on("connection", (socket) => {
-  console.log("user connected");
-
-  socket.on("disconnect", () => {
-    console.log("user disconnected");
-  });
-});
 server.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+io.on("connection",function (socket) {
+  console.log("a user connected");
+  socket.on("disconnect", function() {
+    console.log("user disconnected");
+  });
+  
 });
