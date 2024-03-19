d69288b5bb4db01a65299213104c502055bdce2f
YbarTom <a22tomybanog@inspedralbes.cat>
2024-03-19 10:49:05 +0100

    se empieza cuando hay dos jugadores

--- server/index.js
+++ server/index.js
@@ -4,27 +4,47 @@ const socketIo = require('socket.io');
 const server = http.createServer();
 const io = socketIo(server);
 
+// Objeto para mantener el recuento de jugadores en cada sala
+const roomPlayers = {};
+
 io.on('connection', (socket) => {
     console.log('A user connected');
 
     socket.on('createRoom', (roomName) => {
         socket.join(roomName);
+        roomPlayers[roomName] = 1; // Se crea la sala con un jugador
         console.log(`Room created: ${roomName}`);
-        socket.emit('roomCreated', roomName); // Notify the client that the room has been created
+        socket.emit('roomCreated', roomName); // Notificar al cliente que la sala ha sido creada
     });
 
     socket.on('joinRoom', (roomName) => {
         socket.join(roomName);
+        roomPlayers[roomName] = (roomPlayers[roomName] || 0) + 1; // Incrementar el contador de jugadores en la sala
         console.log(`User joined room: ${roomName}`);
+
+        // Verificar si hay dos jugadores en la sala
+        if (roomPlayers[roomName] === 2) {
+            // Emitir el evento de comenzar partida a todos los jugadores en la sala
+            io.to(roomName).emit('startGame');
+        }
     });
 
     socket.on('message', (data) => {
         console.log('Message from client:', data);
-        io.to(data.room).emit('message', 'Message received: ' + data.text); // Broadcasting the received message to all clients in the room
+        io.to(data.room).emit('message', 'Message received: ' + data.text); // Transmitir el mensaje recibido a todos los clientes en la sala
     });
 
     socket.on('disconnect', () => {
         console.log('User disconnected');
+        // Disminuir el contador de jugadores cuando un usuario se desconecta
+        Object.keys(roomPlayers).forEach((roomName) => {
+            if (socket.rooms.has(roomName)) {
+                roomPlayers[roomName] -= 1;
+                if (roomPlayers[roomName] <= 0) {
+                    delete roomPlayers[roomName]; // Eliminar la sala si no hay jugadores en ella
+                }
+            }
+        });
     });
 });
 
